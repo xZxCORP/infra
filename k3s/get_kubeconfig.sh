fa3c53f15d7b97fc2ece5ee47914acc011cfe8b9
turpin_l <turpin_l@etna-alternance.net>
2024-05-22 13:10:55 +0200

    feat: reconfigure

--- k3s/get_kubeconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source common.sh
-k3sup install \
-    --ip $MASTER_IP \
-    --user $USER \
-    --ssh-key $SSH_KEY \
-    --merge \
-    --skip-install
--- k3s/use.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export KUBECONFIG=$(pwd)/kubeconfig
-kubectl config use-context default
-echo "KUBECONFIG set to $KUBECONFIG_PATH and context switched to 'default'"
