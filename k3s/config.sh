5e3e6a071bebe4eb955c21d304f54bc5f808d12f
turpin_l <turpin_l@etna-alternance.net>
2024-05-24 14:28:15 +0200

    feat: adding nginx app

--- k3s/config.sh
+++ k3s/config.sh
@@ -6,8 +6,3 @@ NODES=($MASTER_IP "${WORKER_IPS[@]}")
 USER="ubuntu"
 SSH_KEY_PATH="~/.ssh/gpe-vm"
 SCRIPT_PATH="~/"
-
-
-GIT_REPO="git@your-private-repo.git"
-GIT_BRANCH="main"
-GIT_REPO_PATH="path/to/your/argo/cd/config"
