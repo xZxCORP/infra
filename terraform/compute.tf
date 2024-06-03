5025fbc62a5d72d646c6559b5e81cfc2ca83f3b1
turpin_l <turpin_l@etna-alternance.net>
2024-06-03 15:37:28 +0200

    feat: adding github runner

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -20,6 +20,28 @@ resource "oci_core_instance" "master" {
 
   display_name = "master"
 }
+resource "oci_core_instance" "runner" {
+  availability_domain = var.availability_domain
+  compartment_id      = var.compartment_id
+  shape               = var.instance_shape
+  shape_config {
+    memory_in_gbs = 6
+    ocpus         = 1
+  }
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.z_subnet.id
+    assign_public_ip = true
+  }
+  metadata = {
+    ssh_authorized_keys = file(var.public_key_path)
+  }
+  source_details {
+    source_type = "image"
+    source_id   = var.instance_image_ocid
+  }
+
+  display_name = "runner"
+}
 
 resource "oci_core_instance" "worker" {
   count               = 2
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "master_public_ip" {
   description = "Public IP address of the master instance."
   value       = oci_core_instance.master.public_ip
 }
+output "runner_public_ip" {
+  description = "Public IP address of the runner instance."
+  value       = oci_core_instance.runner.public_ip
+}
 
 output "worker_public_ips" {
   description = "Public IP addresses of the worker instances."
