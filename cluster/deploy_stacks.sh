0954f5a5d89e68e14e09e258cf5688b41ba449d3
turpin_l <turpin_l@etna-alternance.net>
2024-09-02 17:01:47 +0200

    feat: update infra

--- cluster/deploy_stacks.sh
+++ cluster/deploy_stacks.sh
@@ -28,12 +28,12 @@ rsync_files $MASTER_IP ".env" "~/.env"
 
 
 echo "Déploiement des stacks sur le nœud maître..."
-ssh_exec $MASTER_IP 'export $(xargs < ~/.env) && docker stack deploy -c ~/stacks/traefik/traefik-stack.yml traefik'
-ssh_exec $MASTER_IP 'export $(xargs < ~/.env) && docker stack deploy -c ~/stacks/mysql/mysql-stack.yml mysql'
-ssh_exec $MASTER_IP 'export $(xargs < ~/.env) && docker stack deploy -c ~/stacks/rabbitmq/rabbitmq-stack.yml rabbitmq'
-ssh_exec $MASTER_IP 'export $(xargs < ~/.env) && docker stack deploy -c ~/stacks/minio/minio-stack.yml minio'
-ssh_exec $MASTER_IP 'export $(xargs < ~/.env) && docker stack deploy -c ~/stacks/wheelz/wheelz-stack.yml wheelz'
-ssh_exec $MASTER_IP 'export $(xargs < ~/.env) && docker stack deploy -c ~/stacks/webhook/webhook-stack.yml webhook'
+ssh_exec $MASTER_IP 'set -a; . ~/.env; set +a; docker stack deploy -c ~/stacks/traefik/traefik-stack.yml traefik'
+ssh_exec $MASTER_IP 'set -a; . ~/.env; set +a; docker stack deploy -c ~/stacks/mysql/mysql-stack.yml mysql'
+ssh_exec $MASTER_IP 'set -a; . ~/.env; set +a; docker stack deploy -c ~/stacks/rabbitmq/rabbitmq-stack.yml rabbitmq'
+ssh_exec $MASTER_IP 'set -a; . ~/.env; set +a; docker stack deploy -c ~/stacks/minio/minio-stack.yml minio'
+ssh_exec $MASTER_IP 'set -a; . ~/.env; set +a; docker stack deploy -c ~/stacks/wheelz/wheelz-stack.yml wheelz'
+ssh_exec $MASTER_IP 'set -a; . ~/.env; set +a; docker stack deploy -c ~/stacks/webhook/webhook-stack.yml webhook'
 
 echo "Stacks déployées:"
 ssh_exec $MASTER_IP "docker stack ls"
