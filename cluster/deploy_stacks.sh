d328c02c7d93ff6092f8b67e88bd3d85223b8086
Léo Turpin <turpin_l@etna-alternance.net>
2025-01-23 13:55:32 +0100

    feat: adding postgres

--- cluster/deploy_stacks.sh
+++ cluster/deploy_stacks.sh
@@ -3,6 +3,7 @@ source deploy_base.sh
 echo "Déploiement des stacks sur le nœud maître..."
 ssh_exec $MASTER_IP 'set -a; . ~/.env; set +a; docker stack deploy -c ~/stacks/traefik/traefik-stack.yml traefik'
 ssh_exec $MASTER_IP 'set -a; . ~/.env; set +a; docker stack deploy -c ~/stacks/mysql/mysql-stack.yml mysql'
+ssh_exec $MASTER_IP 'set -a; . ~/.env; set +a; docker stack deploy -c ~/stacks/postgres/postgres-stack.yml postgres'
 ssh_exec $MASTER_IP 'set -a; . ~/.env; set +a; docker stack deploy -c ~/stacks/mongodb/mongodb-stack.yml mongodb'
 ssh_exec $MASTER_IP 'set -a; . ~/.env; set +a; docker stack deploy -c ~/stacks/rabbitmq/rabbitmq-stack.yml rabbitmq'
 ssh_exec $MASTER_IP 'set -a; . ~/.env; set +a; docker stack deploy -c ~/stacks/minio/minio-stack.yml minio'
