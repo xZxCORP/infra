64b66bc0cf693f7d18428be7d259491811213ec4
Léo Turpin <turpin_l@etna-alternance.net>
2025-04-14 20:42:22 +0200

    feat: finish cluster

--- terraform/network.tf
+++ terraform/network.tf
@@ -4,6 +4,40 @@ resource "oci_core_vcn" "z_vcn" {
   display_name   = "z_vcn"
 }
 
+resource "oci_core_internet_gateway" "z_internet_gateway" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.z_vcn.id
+  display_name   = "z_internet_gateway"
+}
+
+resource "oci_core_route_table" "z_route_table" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.z_vcn.id
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    network_entity_id = oci_core_internet_gateway.z_internet_gateway.id
+  }
+
+  display_name = "z_route_table"
+}
+
+resource "oci_core_security_list" "z_security_list" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.z_vcn.id
+  display_name   = "z_security_list"
+
+  egress_security_rules {
+    protocol    = "all"
+    destination = "0.0.0.0/0"
+  }
+
+  ingress_security_rules {
+    protocol = "all"
+    source   = "0.0.0.0/0"
+  }
+}
+
 resource "oci_core_subnet" "z_subnet" {
   cidr_block        = var.subnet_cidr
   compartment_id    = var.compartment_id
@@ -11,49 +45,4 @@ resource "oci_core_subnet" "z_subnet" {
   route_table_id    = oci_core_route_table.z_route_table.id
   security_list_ids = [oci_core_security_list.z_security_list.id]
   display_name      = "z_subnet"
-
-}
-resource "oci_core_internet_gateway" "z_internet_gateway" {
-  compartment_id = var.compartment_id
-  vcn_id         = oci_core_vcn.z_vcn.id
-  display_name   = "z_internet_gateway"
-}
-resource "oci_core_route_table" "z_route_table" {
-  compartment_id = var.compartment_id
-  vcn_id         = oci_core_vcn.z_vcn.id
-  route_rules {
-    destination       = "0.0.0.0/0"
-    network_entity_id = oci_core_internet_gateway.z_internet_gateway.id
-  }
-  display_name = "z_route_table"
-
-}
-resource "oci_core_security_list" "z_security_list" {
-  compartment_id = var.compartment_id
-  vcn_id         = oci_core_vcn.z_vcn.id
-  display_name   = "z_security_list"
-
-  # Rule for allowing all outgoing traffic from the subnet to the Internet
-  egress_security_rules {
-    protocol    = "all"
-    destination = "0.0.0.0/0"
-  }
-
-  # Rule for allowing all traffic within the cluster node subnet
-  ingress_security_rules {
-    protocol = "all"
-    source   = var.subnet_cidr
-  }
-
-  # Rule for external SSH access
-  ingress_security_rules {
-    protocol = "6"
-    source   = "0.0.0.0/0"
-
-    tcp_options {
-      min = 22
-      max = 22
-    }
-  }
-
 }
