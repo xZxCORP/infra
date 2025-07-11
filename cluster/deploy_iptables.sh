0668d3eaf57d919c64832925c028bfb7e2c8cd58
Léo Turpin <turpin_l@etna-alternance.net>
2025-07-11 23:55:35 +0200

    fix: remove old shit wouuuuu

--- cluster/deploy_iptables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source config.sh
-
-for NODE in "${NODES_AND_RUNNER[@]}"; do
-    echo "Configuration des règles iptables sur $NODE"
-    scp -i $SSH_KEY_PATH ./deployers/configure_iptables.sh $USER@$NODE:$SCRIPT_PATH
-    ssh -i $SSH_KEY_PATH $USER@$NODE "sudo bash $SCRIPT_PATH/configure_iptables.sh"
-done
--- cluster/deployers/configure_iptables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-iptables -P INPUT ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -F
-netfilter-persistent save
