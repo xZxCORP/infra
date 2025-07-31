dceb5a990fd249088238a5d5025e0b84974ff8a3
Léo Turpin <turpin_l@etna-alternance.net>
2025-07-31 18:35:27 +0200

    feat: update custom entrypoint script to create additional databases and fix mode formatting in YAML files

--- ansible/config/postgres/custom-entrypoint.sh
+++ ansible/config/postgres/custom-entrypoint.sh
@@ -16,4 +16,15 @@ if ! psql -U "$POSTGRES_USER" -lqt | cut -d \| -f 1 | grep -qw outline; then
     psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE DATABASE outline;"
 fi
 
+if ! psql -U "$POSTGRES_USER" -lqt | cut -d \| -f 1 | grep -qw chain_state_service_db; then
+    psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE DATABASE chain_state_service_db;"
+fi
+
+if ! psql -U "$POSTGRES_USER" -lqt | cut -d \| -f 1 | grep -qw user_service_db; then
+    psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE DATABASE user_service_db;"
+fi
+
+if ! psql -U "$POSTGRES_USER" -lqt | cut -d \| -f 1 | grep -qw auth_service_db; then
+    psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE DATABASE auth_service_db;"
+fi
 wait $POSTGRES_PID
